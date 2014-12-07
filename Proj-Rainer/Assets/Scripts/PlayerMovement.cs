6696f0e663dbfb95bdeb80acc4f196ff795a1671
Dartheon <brian_Sutherland100@hotmail.com>
2014-12-07 02:36:48 -0700

    all is good now
    last little tweeks

--- Proj-Rainer/Assets/Scripts/PlayerMovement.cs
+++ Proj-Rainer/Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class PlayerMovement : MonoBehaviour {
 
 	public float PlayerSpeed = 5f;
-	public float RotationSpeed = 5f;
 
 	// Use this for initialization
 	void Start () 
@@ -16,7 +15,7 @@ public class PlayerMovement : MonoBehaviour {
 	void FixedUpdate () 
 	{
 		float upDown = Input.GetAxis("Vertical") * PlayerSpeed;
-		float leftRight = Input.GetAxis("Horizontal") * RotationSpeed;
+		float leftRight = Input.GetAxis("Horizontal") * PlayerSpeed;
 		transform.Translate(leftRight, 0, upDown);
 	}
 }

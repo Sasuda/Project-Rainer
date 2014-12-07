fd71fb26320502330fb95257c8f9619bf5257993
Dartheon <brian_Sutherland100@hotmail.com>
2014-12-07 01:25:08 -0700

    player movement

---
+++ Proj-Rainer/Assets/Standard Assets/Scripts/General Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public float PlayerSpeed = 5f;
+	public float RotationSpeed = 5f;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+	{
+		float translation = Input.GetAxis("Vertical") * PlayerSpeed;
+		float rotation = Input.GetAxis("Horizontal") * RotationSpeed;
+		transform.Translate(0, 0, translation);
+		transform.Rotate(0, rotation, 0);
+	}
+}
